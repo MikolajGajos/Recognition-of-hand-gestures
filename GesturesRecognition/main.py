91be9738238cf0a242848879f946480c61b73510
Mikolaj <mikus22500@gmail.com>
2023-11-12 16:24:21 +0100

    Implement cross validation

--- GesturesRecognition/main.py
+++ GesturesRecognition/main.py
@@ -6,6 +6,12 @@ from tensorflow import keras
 from keras import layers
 from keras.models import Sequential
 
+import os
+import pandas as pd
+from sklearn import metrics
+from scipy.stats import zscore
+from sklearn.model_selection import KFold
+
 print(tf.config.list_physical_devices('GPU'))
 
 data_dir = 'C:/Users/Mikolaj/.keras/datasets/flower_photos'
@@ -15,97 +21,119 @@ img_height = 160
 img_width = 160
 IMG_SIZE = (img_width, img_height)
 
-train_ds = tf.keras.utils.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset='training',
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
-
-val_ds = tf.keras.utils.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset='validation',
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
-
-class_names = train_ds.class_names
-print(class_names)
-
-AUTOTUNE = tf.data.AUTOTUNE
-
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
-
-data_augmentation = keras.Sequential([
-        layers.RandomFlip("horizontal",
-                          input_shape=(img_height,
-                                       img_width,
-                                       3)),
-        layers.RandomRotation(0.1),
-        layers.RandomZoom(0.1),
-])
-
-num_classes = len(class_names)
+splits = 3
 epochs = 3
-epochs_range = range(epochs)
 
-model = Sequential([
-    data_augmentation,
-    layers.Rescaling(1./255),
-    layers.Conv2D(16, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(32, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(64, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Flatten(),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(num_classes, name="outputs")
-])
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-history = model.fit(
-    train_ds,
-    validation_data=val_ds,
-    epochs=epochs
-)
+dataset = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    shuffle=True,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
 
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
+class_names = dataset.class_names
+num_classes = len(class_names)
 
-loss = history.history['loss']
-val_loss = history.history['val_loss']
 
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
+def create_model():
+    base_model = Sequential([
+        layers.Rescaling(1./255),
+        layers.Conv2D(16, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(32, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(num_classes, name="outputs")
+    ])
+    return base_model
 
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.show()
 
-image_path = "D:/daisy.jpg"
+X = []
+y = []
+for images, labels in dataset:
+    X.append(images.numpy())
+    y.append(labels.numpy())
 
-img = tf.keras.utils.load_img(
-    image_path, target_size=(img_height, img_width)
-)
-img_array = tf.keras.utils.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0)  # Create a batch
+X = np.concatenate(X)
+y = np.concatenate(y)
 
-predictions = model.predict(img_array)
-score = tf.nn.softmax(predictions[0])
+# class_names = dataset.class_names
+#
+# plt.figure(figsize=(10, 10))
+# for i in range(9):
+#     ax = plt.subplot(3, 3, i + 1)
+#     plt.imshow(X[i].astype("uint8"))
+#     plt.title(class_names[y[i]])
+#
+# plt.show()
 
-print(
-    "This image most likely belongs to {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(score)], 100 * np.max(score))
-)
+predicted_y = []
+expected_y = []
+
+kf = KFold(n_splits=splits, shuffle=True, random_state=42)
+
+for train_index, test_index in kf.split(X):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+
+    model = create_model()
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+    history = model.fit(X_train, y_train,
+                        validation_data=(X_test, y_test),
+                        epochs=epochs)
+
+    predictions = model.predict(X_test)
+    predicted_labels = np.argmax(predictions, axis=1)
+
+    accuracy = metrics.accuracy_score(y_test, predicted_labels)
+    expected_y.append(y_test)
+    predicted_y.append(predicted_labels)
+    print(f'Accuracy: {accuracy}')
+
+expected_y = np.concatenate(expected_y)
+predicted_y = np.concatenate(predicted_y)
+accuracy = metrics.accuracy_score(expected_y, predicted_y)
+print(f'Accuracy: {accuracy}')
+
+# epochs_range = range(epochs)
+#
+# acc = history.history['accuracy']
+# val_acc = history.history['val_accuracy']
+#
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
+#
+# plt.figure(figsize=(8, 8))
+# plt.subplot(1, 2, 1)
+# plt.plot(epochs_range, acc, label='Training Accuracy')
+# plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+# plt.legend(loc='lower right')
+# plt.title('Training and Validation Accuracy')
+#
+# plt.subplot(1, 2, 2)
+# plt.plot(epochs_range, loss, label='Training Loss')
+# plt.plot(epochs_range, val_loss, label='Validation Loss')
+# plt.legend(loc='upper right')
+# plt.title('Training and Validation Loss')
+# plt.show()
+#
+# image_path = "D:/sunflower.jpg"
+#
+# img = tf.keras.utils.load_img(
+#     image_path, target_size=(img_height, img_width)
+# )
+# img_array = tf.keras.utils.img_to_array(img)
+# img_array = tf.expand_dims(img_array, 0)  # Create a batch
+#
+# predictions = model.predict(img_array)
+# score = tf.nn.softmax(predictions[0])
+#
+# print(
+#     "This image most likely belongs to {} with a {:.2f} percent confidence."
+#     .format(class_names[np.argmax(score)], 100 * np.max(score))
+# )
