25d264a52a8a0da6baacfb9bdfbf88474dcecc5e
Mikolaj <mikus22500@gmail.com>
2023-11-12 19:06:56 +0100

    Make figure for each fold

--- GesturesRecognition/main.py
+++ GesturesRecognition/main.py
@@ -22,7 +22,7 @@ img_width = 160
 IMG_SIZE = (img_width, img_height)
 
 splits = 3
-epochs = 3
+epochs = 2
 
 dataset = tf.keras.utils.image_dataset_from_directory(
     data_dir,
@@ -74,7 +74,10 @@ expected_y = []
 
 kf = KFold(n_splits=splits, shuffle=True, random_state=42)
 
+fold = 0
 for train_index, test_index in kf.split(X):
+    fold += 1
+
     X_train, X_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
 
@@ -95,33 +98,30 @@ for train_index, test_index in kf.split(X):
     predicted_y.append(predicted_labels)
     print(f'Accuracy: {accuracy}')
 
+    epochs_range = range(epochs)
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    plt.subplot(1, 3, fold)
+    plt.plot(epochs_range, acc, label='Training Accuracy', linestyle='--')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy', linestyle='-')
+    plt.title(f'Fold {fold}')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+plt.tight_layout()  # Adjust the layout to prevent overlap
+plt.show()
+
 expected_y = np.concatenate(expected_y)
 predicted_y = np.concatenate(predicted_y)
 accuracy = metrics.accuracy_score(expected_y, predicted_y)
 print(f'Accuracy: {accuracy}')
 
-# epochs_range = range(epochs)
-#
-# acc = history.history['accuracy']
-# val_acc = history.history['val_accuracy']
-#
-# loss = history.history['loss']
-# val_loss = history.history['val_loss']
-#
-# plt.figure(figsize=(8, 8))
-# plt.subplot(1, 2, 1)
-# plt.plot(epochs_range, acc, label='Training Accuracy')
-# plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-# plt.legend(loc='lower right')
-# plt.title('Training and Validation Accuracy')
-#
-# plt.subplot(1, 2, 2)
-# plt.plot(epochs_range, loss, label='Training Loss')
-# plt.plot(epochs_range, val_loss, label='Validation Loss')
-# plt.legend(loc='upper right')
-# plt.title('Training and Validation Loss')
-# plt.show()
-#
 # image_path = "D:/sunflower.jpg"
 #
 # img = tf.keras.utils.load_img(
